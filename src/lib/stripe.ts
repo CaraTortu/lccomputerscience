1badac585d4e2c5980adb906dd3465b669577de5
CaraTortu <javierdj@jadk.com>
2025-03-24 21:59:26 +0000

    fix: tRPC stabilised batchHTTPLink

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,9 +4,7 @@ import { env } from "~/env";
 let stripe: Stripe | null = null;
 
 export const getStripe = async () => {
-    if (!stripe) {
-        stripe = await loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-    }
+    stripe ??= await loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
     return stripe;
 };
